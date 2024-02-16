ed083a4f8ec4a942e1c0f3147ebd8e5815605eaf
Sudharaka <sudharakafernandobusiness@gmail.com>
2024-02-16 22:19:24 +0530

    Added functionality to automate the download of video

--- image_to_video.py
+++ image_to_video.py
@@ -1,14 +1,16 @@
 import requests
 
-response = requests.post(
-    f"https://api.stability.ai/v2alpha/generation/image-to-video",
-    headers={"authorization": f"Bearer sk-kxMvqmynYxEvqvkqvHwfUOJ5b96gtsNsYdrMcWcwscuSqRkH"},
-    files={"image": open("./rainy_day.jpg", "rb")},
-    data={
-        "seed": 0,
-        "cfg_scale": 1.8,
-        "motion_bucket_id": 127
-    },
-)
+image_location = "./rainy_day.jpg"
+def generate_video_id(image_location):
+    response = requests.post(
+        f"https://api.stability.ai/v2alpha/generation/image-to-video",
+        headers={"authorization": f"Bearer sk-kxMvqmynYxEvqvkqvHwfUOJ5b96gtsNsYdrMcWcwscuSqRkH"},
+        files={"image": open(image_location, "rb")},
+        data={
+            "seed": 0,
+            "cfg_scale": 1.8,
+            "motion_bucket_id": 127
+        },
+    )
+    return response
 
-print(response.json())
