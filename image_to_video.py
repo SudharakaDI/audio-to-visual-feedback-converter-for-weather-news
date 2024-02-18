9cb7989265a805e4a6b3cd8368ce1be0213008b2
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2024-02-18 12:05:54 +0530

    resources added

--- image_to_video.py
+++ image_to_video.py
@@ -3,7 +3,7 @@ import requests
 response = requests.post(
     f"https://api.stability.ai/v2alpha/generation/image-to-video",
     headers={"authorization": f"Bearer sk-kxMvqmynYxEvqvkqvHwfUOJ5b96gtsNsYdrMcWcwscuSqRkH"},
-    files={"image": open("./rainy_day.jpg", "rb")},
+    files={"image": open("./images/mountains/mist.jpg", "rb")},
     data={
         "seed": 0,
         "cfg_scale": 1.8,
